b21885d56e014bdb019d832993915b4303b34d96
Angel-ISO <angelgabrielorteg@gmail.com>
2023-06-29 09:29:00 -0500

    Γëí╞Æ├£├ç ll

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
-﻿﻿internal class Program
+﻿﻿﻿internal class Program
 {
     private static void Main(string[] args)
     {
         LinqQueries queries = new LinqQueries();
-         ImprimirValores(queries.AllCollection());
-       
+        ImprimirValores(queries.GetBooksTake());
+        //ImprimirValores(queries.LibrosDespues2000());
+        //ImprimirValores(queries.GetBooksTake());
+        //ImprimirValores(queries.GetBooksSkipTerceyCuarto());
+        // Console.WriteLine($"Total de libros que tienen entre 200 y 500 paginas {queries.GetBooksCount()}");
+        // Console.WriteLine($"Fecha de publicacion menor {queries.GetBooksDatePublishMax()}");
+        //ImprimirValores(queries.GetBooksListDatePublishMinor());
     }
 
     private static void ImprimirValores(IEnumerable<Book> books)
     {
         int registros = 0;
         Console.Clear();
-        Console.ForegroundColor = ConsoleColor.Magenta;
         Console.WriteLine("{0,-70} {1,7} {2,20}\n", "Titulo", "N. Paginas", "Fecha publicacion");
         foreach (Book book in books)
         {
